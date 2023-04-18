bc3053ada163e804766906f5a75b77cac95e2918
Andres <salamancaclaudioandres@gmail.com>
2023-04-18 18:04:41 -0300

    create directory ec2_user_data

---
+++ ec2_user_data/main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "examplesg" {
+  name        = "pin2204-sg"
+  description = "Allow ssh traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "ec2_instance" {
+  #ami                   = "ami-0557a15b87f6559cf"  #Ubuntu AMI where doesn't  work user_data 
+  ami                    = "ami-00c39f71452c08778"  # Amazon AMI where work user_data
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.examplesg.id}"]
+  key_name = "linuxaws"
+
+ user_data = <<-EOF
+             #!/bin/bash
+              echo "Hello, Andres" > /home/ec2-user/hello.txt
+              EOF
+#  user_data = "${file("ec2_user_data.sh")}"
+
+  tags = {
+    Name = "ec2-pin2204"
+  }
+}
+ 
+ output "public_ip" {
+   value = aws_instance.ec2_instance.public_ip
+
+   }
+
